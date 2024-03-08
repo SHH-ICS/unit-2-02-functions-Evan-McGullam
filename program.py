aea68412d287e01a2ef93bd1d23c288b6daf5d7f
Evan-McGullam <159149912+Evan-McGullam@users.noreply.github.com>
2024-03-08 13:41:17 +0000

    finshed

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction(): 
   # My Function
+  print("Hello World!")
+
+myFunction()
